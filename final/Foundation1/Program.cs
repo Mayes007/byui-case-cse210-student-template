d9739efef2bf6a226cf5bb6a907e7e7498928404
Mayes007 <may23006@byui.edu>
2025-03-29 15:00:25 -0600

    main

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -1,9 +1,56 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+class Comment
 {
-    static void Main(string[] args)
+    public string CommenterName { get; set; }
+    public string Text { get; set; }
+
+    public Comment(string commenterName, string text)
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        CommenterName = commenterName;
+        Text = text;
+        }
+    public void Display()
+    {
+        Console.WriteLine($"{CommenterName}: {Text}");
     }
 }
+class Video
+{
+    public string Title { get; set; }
+    public string Uploader { get; set; }
+    public int Length { get; set; } // in seconds
+    private List<Comment> Comments;
+
+    public Video(string title, string uploader, int length)
+    {
+        Title = title;
+        Uploader = uploader;
+        Length = length;
+        Comments = new List<Comment>();
+    }
+
+    public void AddComment(Comment comment)
+    {
+        Comments.Add(comment);
+    }   
+
+    public int GetCommentCount()
+    {
+        return Comments.Count;
+    }
+    public void Display()
+    {
+        Console.WriteLine($"Title: {Title}");
+        Console.WriteLine($"Uploader: {Uploader}");
+        Console.WriteLine($"Length: {Length} seconds");
+        Console.WriteLine("Comments:");
+        
+        foreach (var comment in Comments)
+        {
+            comment.Display();
+        }
+    }
+
+    }
